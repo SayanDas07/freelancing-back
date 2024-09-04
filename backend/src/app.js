35ed9a7ac4211302ed7c6fb6b32f4c1ea8ccdff8
Sayan Das <sayandasraja56@gmail.com>
2024-09-04 13:46:16 +0530

    edit app

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,26 +21,23 @@ app.use(cors(corsOptions))
 import userRouter from './routes/user.routes.js';
 
 app.use('/api/v1/user', userRouter);
-//http://localhost:8000/api/v1/user/register
 
 import resumeRouter from './routes/resume.routes.js';
 
 app.use('/api/v1/resume', resumeRouter);
-//http://localhost:8000/api/v1/resume/upload-resume
 
 import offerRouter from './routes/freelanceOffer.routes.js';
 
 app.use('/api/v1/offer', offerRouter);
-//http://localhost:8000/api/v1/offer/create-offer
+
 
 import applicationRouter from './routes/application.routes.js';
 
 app.use('/api/v1/application', applicationRouter);
-//http://localhost:8000/api/v1/application/apply
 
 import ratingRouter from './routes/rating.routes.js';
 
 app.use('/api/v1/rating', ratingRouter);
-//http://localhost:8000/api/v1/rating/rate
+
 
 export { app };
